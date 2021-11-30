6860bb14034fe215857ef4a9ec35e78e3b6747ed
‘jiangyue’ <jiangyue03@qiyi.com>
2021-11-30 12:05:40 +0800

    feat: test

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+enum vendingMachineError: Error {
+    case inva
+    case insu
+    case outo
+}
+
+struct Item {
+    var price: Int
+    vat count: Int
+}
+
+class Vending {
+    func vend() tow
+}
